a66d6c5e31076e83810f3836e02157a85353f2fa
Simon Mika <simon@mika.se>
2025-04-10 17:44:30 +0200

    Broke out Me.Organization.

---
+++ Me/Organization.ts
@@ -0,0 +1,16 @@
+import { cryptly } from "cryptly"
+import { isly } from "isly"
+import { User } from "User"
+
+export interface Organization {
+	id: cryptly.Identifier4
+	permissions: User.Permissions
+}
+export namespace Organization {
+	export const { type, is, flawed } = isly
+		.object({
+			id: isly.from("cryptly.Identifier4", cryptly.Identifier4.is),
+			permissions: User.Permissions.type,
+		})
+		.bind()
+}
--- Me/index.ts
+++ Me/index.ts
@@ -1,22 +1,16 @@
-import { cryptly } from "cryptly"
 import { isly } from "isly"
-import { User } from "../User"
+import { Organization as _Organization } from "./Organization"
 
 export interface Me {
 	email: string
-	organizations: { id: cryptly.Identifier4; name: string; permissions: User.Permissions }[]
+	organizations: Me.Organization[]
 }
 export namespace Me {
+	export import Organization = _Organization
 	export const { type, is, flawed } = isly
 		.object<Me>({
 			email: isly.string(),
-			organizations: isly
-				.object({
-					id: isly.from("cryptly.Identifier4", cryptly.Identifier4.is),
-					name: isly.string(),
-					permissions: User.Permissions.type,
-				})
-				.array(),
+			organizations: Organization.type.array(),
 		})
 		.bind()
 }
